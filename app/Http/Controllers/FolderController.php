27382fc4e69d3c7f7eee677280596b13a6dcb105
t.suzuki <tkft.psy@gmail.com>
2020-07-03 14:36:29 +0900

    firstcommit

---
+++ app/Http/Controllers/FolderController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Folder;
+use Illuminate\Http\Request;
+use App\Http\Requests\CreateFolder;
+use Illuminate\Support\Facades\Auth;
+
+
+class FolderController extends Controller
+{
+    public function create(CreateFolder $request)
+    {
+        // フォルダモデルのインスタンスを作成する
+        $folder = new Folder();
+        // タイトルに入力値を代入する
+        $folder->title = $request->title;
+        Auth::user()->folders()->save($folder);
+
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
+    }
+    public function showCreateForm()
+    {
+        return view('folders/create');
+    }
+}
